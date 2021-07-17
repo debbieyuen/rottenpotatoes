07b09ff25f7a2c0557f33b1f8b99ecbe75e02d1b
Debbie Yuen <deborahyuen2016@berkeley.edu>
2021-07-17 03:14:04 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,11 +3,33 @@ class MoviesController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
+    #call functions
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = Movie.all
+     
+     if params[:ratings].nil?
+      @ratings_to_show = Movie.all_ratings
+     else 
+      @ratings_to_show = params[:ratings]
+     end
+    
+    @sort_by = params[:sort_by]
+    
+    @some_var = @sort_by ? "hilite p-3 mb-2 bg-warning": "None" 
+    if params[:ratings]
+      @movies = Movie.with_ratings(params[:ratings]).order(@sort_by)
+      #.order picks all the movies. it will order them in a correct way. get it from the views
+       #params sort by params[:sort_by] and get information 
+      #use movies_path make a call to your data base @movie = Movie.order
+      #plug this information into order 
+      #tell data base to go to Movie data base and call which method on it. And whatver methods
+      #gives you, which is going to be a collection of movies. Now order them by what i specify
+    else
+      @movies = Movie.all
+    end
+    @all_ratings = Movie.all_ratings
   end
 
   def new
